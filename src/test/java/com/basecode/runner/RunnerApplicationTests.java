bd222955568f76be02eb54c2b182d14b341b6c41
Felipe Pires Morandini <felipepiresmorandini@gmail.com>
2024-11-23 21:49:53 -0300

    First Commit

---
+++ src/test/java/com/basecode/runner/RunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basecode.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
